3160b5275a70401e5ffe91cd006e47998da484d4
Chulumanco Dana <chuludana@gmail.com>
2025-02-06 09:28:52 +0200

    Calculate standard deviation

--- Practical ome.R
+++ Practical ome.R
@@ -7,3 +7,7 @@ na_rows = airquality[rowSums(is.na(airquality))>0,]
 #Calculate means
 mean_ozone = mean(airquality$ozone,na.rm=TRUE)
 mean_temp  = mean(airquality$Temp,na.rm=TRUE)
+
+#Calculate standard deviations
+sd_ozone = sd(airquality$Ozone,na.rm=TRUE)
+sd_temp  = sd(airquality$Temp,na.rm=TRUE)
